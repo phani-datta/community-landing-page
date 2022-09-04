7ab6887602281029725d1ecdf8ee2fc99cffd97f
datta9 <datta.yh@gmail.com>
2022-09-04 12:53:04 +0530

    Added Footer, Credits, About Image

---
+++ src/components/Drawer/DrawerCredits.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DrawerCredits = () => {
+    return (
+        <div className="bg-[#f7fafb] p-4 absolute bottom-0 w-fit">
+            <p>Credits:</p>
+            <p><a href="https://www.freepik.com/free-vector/happy-woman-chatting-with-friends-online_12291106.htm">Image by pch.vector</a> on Freepik</p>
+            <p><a href="https://www.freepik.com/free-vector/teamwork-concept-landing-page_5155707.htm">Image by pikisuperstar</a> on Freepik</p>
+        </div>
+    );
+};
+
+export default DrawerCredits;
