7f5c34a8f791ef842c915d6813c8e8f2cdf64cfa
datta9 <datta.yh@gmail.com>
2022-09-05 12:58:29 +0530

    hero mobile view

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,16 +14,16 @@ const Hero = () => {
                     <img src="/assets/images/hero-image.png" className="md:max-w-3xl sm:max-w-sm" />
                     <div>
                         <div className="content__container">
-                            <h1 className="content__container__text text-5xl font-bold">Drixico</h1>
+                            <h1 className="content__container__text">Drixico</h1>
                             <ul className="content__container__list">
-                                <li className="content__container__list__item items-end">Chat <HiChatAlt2 className="text-5xl text-sky-800 ml-1" /></li>
-                                <li className="content__container__list__item items-end">Events <AiFillVideoCamera className="text-5xl text-sky-800 ml-1" /></li>
-                                <li className="content__container__list__item items-baseline">Meets <SiHandshake className="text-4xl text-sky-800 ml-1" /></li>
-                                <li className="content__container__list__item items-baseline">Groups <HiUserGroup className="text-4xl text-sky-800 ml-1" /></li>
+                                <li className="content__container__list__item items-baseline md:items-end">Chat <HiChatAlt2 className="text-3xl md:text-5xl text-sky-800 ml-1" /></li>
+                                <li className="content__container__list__item items-baseline md:items-end">Events <AiFillVideoCamera className="text-2xl md:text-5xl text-sky-800 ml-1" /></li>
+                                <li className="content__container__list__item items-baseline">Meets <SiHandshake className="text-2xl md:text-4xl text-sky-800 ml-1" /></li>
+                                <li className="content__container__list__item items-baseline">Groups <HiUserGroup className="text-2xl md:text-4xl text-sky-800 ml-1" /></li>
                             </ul>
                         </div>
 
-                        <h2 className="text-2xl text-center md:text-start">Large community with huge amounts of talent to share, learn & grow!</h2>
+                        <h2 className="text-xl md:text-2xl text-center md:text-start">Large community with huge amounts of talent to share, learn & grow!</h2>
                         <HeroStats />
                         <button className="btn btn-primary hidden md:block mt-6">Join Now</button>
                         <div className="md:hidden text-center">
